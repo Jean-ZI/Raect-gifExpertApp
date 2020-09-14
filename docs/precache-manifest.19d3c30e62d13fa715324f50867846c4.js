84e02487928fe6067f8c675de08e2daf56adfb51
Jean Zuñiga <jean.zu.iga@gmail.com>
2020-09-14 11:39:23 -0500

    docs agregado

---
+++ docs/precache-manifest.19d3c30e62d13fa715324f50867846c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1deacd0691b3448279fed68b449e7565",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08f969ae06f99ef5afbf",
+    "url": "/static/css/main.c106abee.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08f969ae06f99ef5afbf",
+    "url": "/static/js/main.16a15f4b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
